4a87555df69b841018afca71a9ded4ca34d41cb4
Oleg Budaev <t0r1n88@mail.ru>
2019-10-08 10:01:24 +0900

    create login_page and create test for her step_4_2_8

--- pages/locators.py
+++ pages/locators.py
@@ -1,5 +1,11 @@
 from selenium.webdriver.common.by import By
 
 class MainPageLocators():
+    """
+    Локаторы для главной страницы сайта
+    """
     LOGIN_LINK = (By.CSS_SELECTOR,'#login_link')
     
+class LoginPageLocators():
+    LOGIN_REGISTER_FORM = (By.CSS_SELECTOR, '#register_form')
+    LOGIN_FORM = (By.CSS_SELECTOR, '#login_form')
