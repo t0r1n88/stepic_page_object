af7da7c21cabb1b1a8a95a92471138a8e3aa3d7b
Oleg Budaev <t0r1n88@mail.ru>
2019-10-08 09:36:25 +0900

    fix invalid link step_4_2_7

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,5 +1,6 @@
 from .base_page import BasePage
 from selenium.webdriver.common.by import By
+from .locators import MainPageLocators
 
 
 class MainPage(BasePage):
@@ -10,11 +11,11 @@ class MainPage(BasePage):
         """
         Метод осуществляющий поиск и нажатие кнопки логина
         """
-        link = self.browser.find_element(By.CSS_SELECTOR,'#login_link')
+        link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
         link.click()
     
     def should_be_login_link(self):
         """
         Проверка налиичия ссылки
         """
-        assert self.is_element_present(By.CSS_SELECTOR, "#login_link"),'Login link is not presented'
+        assert self.is_element_present(*MainPageLocators.LOGIN_LINK),'Login link is not presented'
--- test_main_page.py
+++ test_main_page.py
@@ -1,7 +1,7 @@
 from .pages.main_page import MainPage
 import pytest
 
-@pytest.mark.skip
+
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)# инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
