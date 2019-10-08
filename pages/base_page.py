8c820d0f3a8eb8cc0b8c990c10e44e69ca846052
Oleg Budaev <t0r1n88@mail.ru>
2019-10-08 09:30:32 +0900

    Create base and main page

---
+++ pages/base_page.py
@@ -0,0 +1,16 @@
+class BasePage():
+    """
+    Базовый класс
+    Конструктор экземпляра класса принимает на вход:
+    browser: объект драйвера нужного браузера
+    url: ссылка на страницу которую нужно открыть
+    """
+    def __init__(self,browser,url):
+        self.browser = browser
+        self.url = url
+    
+    def open(self):
+    """
+    Метод для открытия страницы по ссылке
+    """
+        self.browser.get(self.url)
